6aa5345ae2be98e100b5efffa386cbb33993455f
Sparrow1029 <19390913+Sparrow1029@users.noreply.github.com>
2022-12-07 06:08:58 -0700

    Day 05 still in progress

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -1,25 +1,38 @@
 use crate::shared::read_lines;
-use regex;
+
+const NUM_STACKS: usize = 9;
+const START_CRATES: usize = 8;
+const MAX_CRATES: usize = NUM_STACKS * START_CRATES;
 
 type Stacks = Vec<Stack>;
 
 #[derive(Debug, Copy, Clone)]
 struct Stack {
-    crates: [u8; 56],
+    crates: [u8; MAX_CRATES],
     top_idx: usize,
 }
 
 impl Stack {
     fn new() -> Self {
         Stack {
-            crates: [0; 56],
-            top_idx: 0,
+            crates: [0; MAX_CRATES],
+            top_idx: START_CRATES - 1, // index
         }
     }
     fn stack_on_top() {}
+
     fn add(&mut self, item: u8) {
-        self.top_idx += 1;
+        self.top_idx -= 1;
         self.crates[self.top_idx] = item;
+    }
+
+    fn reset_top_idx(&mut self) {
+        for (i, val) in self.crates.iter().enumerate() {
+            if *val == 0u8 {
+                self.top_idx = i - 1;
+                break;
+            }
+        }
     }
 }
 
@@ -28,21 +41,28 @@ fn parse_input() -> Stacks {
         .expect("error reading input file")
         .map(|l| l.expect("error reading line"));
 
-    let re = regex::Regex::new(r"\[|\]\s|\]").unwrap();
+    let mut stacks: Stacks = vec![Stack::new(); 9];
+
     let mut cur_line = 1;
     for line in lines {
         if cur_line < 9 {
-            let data = line.bytes();
-            for i in [0..]
-                let crate_ = slice.take(3).filter(|b| ![91, 93].contains(b));
+            let mut iter = line.bytes().array_chunks::<4>();
+            let mut cur_stack_idx = 0;
+            for [_, c, _, _] in &mut iter {
+                match c {
+                    b' ' => stacks[cur_stack_idx].add(0),
+                    _ => stacks[cur_stack_idx].add(c),
+                }
+                cur_stack_idx += 1
             }
-            println!("{data:?}");
+            let last_val = iter.into_remainder().unwrap().nth(1).unwrap();
+            stacks[cur_stack_idx].add(last_val);
             cur_line += 1;
-        } else {
-            break;
+        }
+        for stack in &stacks {
+            println!("{stack:?}");
         }
     }
-    let mut stacks: Stacks = vec![Stack::new(); 9];
     stacks
 }
 
