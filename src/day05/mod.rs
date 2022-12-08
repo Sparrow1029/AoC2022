82bcf6e7ae5f8e8b6ff265a641e6bba13caecb26
Alexander Ray <awray2@es.net>
2022-12-07 19:46:47 -0700

    Day 05 done. Added a VS Code debug config

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -4,68 +4,115 @@ const NUM_STACKS: usize = 9;
 const START_CRATES: usize = 8;
 const MAX_CRATES: usize = NUM_STACKS * START_CRATES;
 
-type Stacks = Vec<Stack>;
+type Stacks = [[u8; MAX_CRATES]; NUM_STACKS];
+type Move = (usize, usize, usize);
 
-#[derive(Debug, Copy, Clone)]
-struct Stack {
-    crates: [u8; MAX_CRATES],
-    top_idx: usize,
-}
-
-impl Stack {
-    fn new() -> Self {
-        Stack {
-            crates: [0; MAX_CRATES],
-            top_idx: START_CRATES - 1, // index
-        }
-    }
-    fn stack_on_top() {}
-
-    fn add(&mut self, item: u8) {
-        self.top_idx -= 1;
-        self.crates[self.top_idx] = item;
-    }
-
-    fn reset_top_idx(&mut self) {
-        for (i, val) in self.crates.iter().enumerate() {
-            if *val == 0u8 {
-                self.top_idx = i - 1;
-                break;
-            }
-        }
-    }
-}
-
-fn parse_input() -> Stacks {
+/// Take the first 8 lines of the input, and read them as bytes
+/// to be inserted into arrays
+fn populate_stacks(stacks: &mut Stacks) {
     let lines = read_lines("src/day05/input.txt")
         .expect("error reading input file")
         .map(|l| l.expect("error reading line"));
 
-    let mut stacks: Stacks = vec![Stack::new(); 9];
-
-    let mut cur_line = 1;
-    for line in lines {
-        if cur_line < 9 {
-            let mut iter = line.bytes().array_chunks::<4>();
-            let mut cur_stack_idx = 0;
-            for [_, c, _, _] in &mut iter {
-                match c {
-                    b' ' => stacks[cur_stack_idx].add(0),
-                    _ => stacks[cur_stack_idx].add(c),
+    let mut cur_top_pos = 8;
+    for line in lines.take(8) {
+        cur_top_pos -= 1;
+        let mut iter = line.bytes().skip(1);
+        stacks[0][cur_top_pos] = iter.nth(0).unwrap();
+        for i in 1..9 {
+            if let Some(val) = iter.nth(3) {
+                match val {
+                    32 => continue,
+                    _ => stacks[i][cur_top_pos] = val,
                 }
-                cur_stack_idx += 1
             }
-            let last_val = iter.into_remainder().unwrap().nth(1).unwrap();
-            stacks[cur_stack_idx].add(last_val);
-            cur_line += 1;
-        }
-        for stack in &stacks {
-            println!("{stack:?}");
         }
     }
+}
+
+/// For the rest of the input, output a vector of tuples containing
+/// (number of crates to move, stack to move from, stack to move to)
+fn get_moves() -> Vec<Move> {
+    let mut moves = vec![];
+    read_lines("src/day05/input.txt")
+        .expect("error reading input file")
+        .map(|l| l.expect("error reading line"))
+        .skip(10)
+        .for_each(|line| {
+            let values = line
+                .split(' ')
+                .skip(1)
+                .step_by(2)
+                .map(|c| c.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>();
+            if let [amt, from, to] = values[..] {
+                moves.push((amt, from - 1, to - 1));
+            }
+        });
+    moves
+}
+
+/// Get index of the first '0' value in one of the crate stacks
+fn get_top_idx(stack: &[u8]) -> usize {
+    stack.iter().position(|v| *v == 0).unwrap()
+}
+
+/// Move values around in the existing crate arrays
+/// assign a vector to hold the values in the interim
+fn move_crates(stacks: &mut Stacks, move_cmd: Move, part: usize) {
+    let (amt, from, to) = move_cmd;
+    let top_idx_from = get_top_idx(&stacks[from]);
+    let top_idx_to = get_top_idx(&stacks[to]);
+
+    let mut crates_to_move: Vec<u8> = vec![];
+    if part == 1 {
+        for i in ((top_idx_from - amt)..top_idx_from).rev() {
+            crates_to_move.push(stacks[from][i]);
+            stacks[from][i] = 0;
+        }
+    } else if part == 2 {
+        for i in top_idx_from - amt..top_idx_from {
+            crates_to_move.push(stacks[from][i]);
+            stacks[from][i] = 0;
+        }
+    }
+    for (i, val) in crates_to_move.iter().enumerate() {
+        stacks[to][top_idx_to + i] = *val;
+    }
+}
+
+/// Get all the u8 values of the top crates for each stack,
+/// convert them to `char`s and collect them into a `String`
+fn get_top_crates(stacks: &Stacks) -> String {
     stacks
+        .iter()
+        .map(|arr| arr[get_top_idx(arr) - 1] as char)
+        .collect::<String>()
+}
+
+pub fn part1() {
+    let mut stacks = [[0; MAX_CRATES]; NUM_STACKS];
+    populate_stacks(&mut stacks);
+    let moves = get_moves();
+    for move_cmd in moves {
+        move_crates(&mut stacks, move_cmd, 1);
+    }
+
+    println!("Part 1: {}", get_top_crates(&stacks));
+}
+
+pub fn part2() {
+    let mut stacks = [[0; MAX_CRATES]; NUM_STACKS];
+    populate_stacks(&mut stacks);
+    let moves = get_moves();
+    for move_cmd in moves {
+        move_crates(&mut stacks, move_cmd, 2);
+    }
+
+    println!("Part 2: {}", get_top_crates(&stacks));
 }
 
 pub fn run() {
-    parse_input();
+    part1();
+    part2();
 }
